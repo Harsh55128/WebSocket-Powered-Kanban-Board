52c955963715dd8c42bf7b95aa7f334375e3a697
HARSH VARDHAN <rajpootharsh895@gmail.com>
2026-02-07 09:49:57 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -95,6 +95,7 @@ io.on("connection", async (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
